96890b3b02b9161052cf003a8f507854829444ad
Eliezer <eliezerhc456@gmail.com>
2025-04-02 16:52:08 -0500

    agregando cambios

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from './shared/components/layout/layout.component';
+
+export const routes: Routes = [
+    {
+        path:'admin',component:LayoutComponent
+    }
+];
