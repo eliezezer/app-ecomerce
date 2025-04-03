d7c0a345724c855d8953d87d365e879eff8a4f01
Eliezer <eliezerhc456@gmail.com>
2025-04-03 16:17:06 -0500

    creando componente product-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './shared/components/layout/layout.component';
+import { ProductListComponent } from './components/producto/product-list/product-list.component';
 
 export const routes: Routes = [
     {
-        path:'admin',component:LayoutComponent
+        path:'admin',component:LayoutComponent,children:[
+            {path:'product-list',component:ProductListComponent}
+        ]
     }
 ];
--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
